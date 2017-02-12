53a51ae782f7a6dc30bc8d35ae87b6746bc57adc
diesieben07 <diesieben07@gmail.com>
2017-02-12 22:57:31 +0100

    initial commit

---
+++ src/main/java/com/example/Java6Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Java6Main {
+
+    public static void main(String[] args) {
+        System.out.println("I am not running Java 8!");
+    }
+
+}
---
+++ src/main/java/com/example/Java8Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Java8Main {
+
+    public static void main(String[] args) {
+        System.out.println("I am running Java 8");
+    }
+
+}
